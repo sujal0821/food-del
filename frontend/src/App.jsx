807c71beec3f85c410aacd29edb45d3f7f66a199
sujal0821 <sujal0821@gmail.com>
2024-08-27 19:03:44 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/cart/Cart'
+import Home from './pages/home/Home'
+import PlaceOrder from './pages/placeorder/PlaceOrder'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favourite food here</h2>
+        <p>Let’s elevate your dining experience—select your meal and savor the journey, one delicious bite at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
