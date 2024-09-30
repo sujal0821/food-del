a5dd8bac11e15b3b166f176847e0e16d3355fdb6
sujal0821 <sujal0821@gmail.com>
2024-09-30 23:31:21 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Cart from './pages/cart/Cart'
-import Home from './pages/home/Home'
-import PlaceOrder from './pages/placeorder/PlaceOrder'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/cart/Cart";
+import Home from "./pages/home/Home";
+import PlaceOrder from "./pages/placeorder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
