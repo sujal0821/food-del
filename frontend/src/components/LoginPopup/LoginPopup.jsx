f7daf23832b064d091793cfa258d4a8a014566dc
sujal0821 <sujal0821@gmail.com>
2024-10-07 20:11:14 +0530

    updated popup

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -4,15 +4,31 @@ import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setshowLogin}) => {
 
-    const[currState,setCurrState] = useState("Sign Up")
+    const[currState,setCurrState] = useState("Login")
 
   return (
-    <div className='loginpopup'>
+    <div className='login-popup'>
       <form className="login-popup-container">
         <div className="login-popup-title">
             <h2>{currState}</h2>
             <img onClick={()=>setshowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
+        <div className="login-popup-inputs">
+          {currState==="Login" ? <></> : <input type="text" placeholder='Your name' required />}
+          <input type="email" placeholder='Your email' required />
+          <input type="text" placeholder='Password' required />
+        </div>
+        <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By contining, i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState==="Login"
+        ?<p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
+        :<p>Already have an account? <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+      }
+        
+        
       </form>
     </div>
   )
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import {StoreContext} from '../../context/StoreContext'
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div className="cart-item-title cart-items-item" key={item._id}>
+                <img src={item.image} alt={item.name} />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price * cartItems[item._id]}</p>
+                <p onClick={() => removeFromCart(item._id)}>x</p>
+              </div>
+            );
+          }
+          return null; // Ensure nothing is rendered if the condition fails
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
