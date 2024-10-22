965f23d4712941866bc0d6e9c9c5ee0c63a86ac5
sujal0821 <sujal0821@gmail.com>
2024-10-22 08:12:13 +0530

    connected back to front

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,31 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import axios from "axios"
 
 const Add = () => {
+
+  const url ="http://localhost:4000"
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name",data.name)
+    formData.append("description",data.description)
+    formData.append("price",Number(data.price))
+    formData.append("category",data.category)
+    formData.append("image",image)
+    const response = await axios.post(`${url}/api/food/add`,formData);
+    if (response.sucess){
+      setData({
+        name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+      })
+      setImage(false)
+    }else{
+
+    }
+  };
+
   return (
     <div>
       <div className="add">
-        <form className="flex-col">
+        <form className="flex-col" onSubmit={onSubmitHandler}>
           <div className="add-img-upload flex-col">
             <p>Upload Image</p>
             <label htmlFor="image">
-              <img src={assets.upload_area} alt="" />
+              <img
+                src={image ? URL.createObjectURL(image) : assets.upload_area}
+                alt=""
+              />
             </label>
-            <input type="text" id="image" hidden required />
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="image"
+              hidden
+              required
+            />
           </div>
           <div className="add-product-name flex-col">
             <p>Product name</p>
-            <input type="text" name="name" placeholder='Type here'/>
+            <input
+              onChange={onChangeHandler}
+              value={data.name}
+              type="text"
+              name="name"
+              placeholder="Type here"
+            />
           </div>
           <div className="add-product-description flex-col">
-            <p>Product desciption</p>
-            <textarea name="descripotion" rows= "6" placeholder="Write content here" required></textarea>
+            <p>Product description</p>
+            <textarea
+              onChange={onChangeHandler}
+              value={data.description}
+              name="description"
+              rows="6"
+              placeholder="Write content here"
+              required
+            ></textarea>
           </div>
           <div className="add-category-price">
             <div className="add-category flex-col">
               <p>Product category</p>
-              <select name="category">
+              <select onChange={onChangeHandler} name="category">
                 <option value="Salad">Salad</option>
                 <option value="Rolls">Rolls</option>
                 <option value="Deserts">Deserts</option>
@@ -38,10 +99,18 @@ const Add = () => {
             </div>
             <div className="add-price flex-col">
               <p>Product price</p>
-              <input type="number" name="price" placeholder="$20"/>
+              <input
+                onChange={onChangeHandler}
+                value={data.price}
+                type="number"
+                name="price"
+                placeholder="$20"
+              />
             </div>
           </div>
-          <button type="Submit" className="add-btn">Add</button>
+          <button type="Submit" className="add-btn">
+            Add
+          </button>
         </form>
       </div>
     </div>
@@ -49,3 +118,5 @@ const Add = () => {
 };
 
 export default Add;
+
+//5.26
