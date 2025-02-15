807c0176a140b7740e7bbd0d1e92d9a8a2c0142d
sujal0821 <sujal0821@gmail.com>
2025-02-15 19:41:46 +0530

    updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,8 @@ import { response } from "express";
 import orderModel from "../models/orderModel.js";
 import userModel from '../models/userModel.js'
 import Stripe from "stripe"
+import dotenv from "dotenv";
+
 
 const stripe =new Stripe(process.env.STRIPE_SECRET_KEY)
 
